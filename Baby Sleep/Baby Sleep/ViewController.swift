607ed3a7fdea17e743a2069381fadd6ca77102c5
Aleksandr <Serov_ae@list.ru>
2020-07-11 13:55:02 +0500

    Create a project

---
+++ Baby Sleep/Baby Sleep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Baby Sleep
+//
+//  Created by Aleksandr Serov on 10.07.2020.
+//  Copyright © 2020 Aleksandr Serov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
