80ac883f9f6e88d85a48de0001b2510dbaa44387
Aleksandr <Serov_ae@list.ru>
2020-07-12 13:31:18 +0500

    Create Model for Collection

---
+++ Baby Sleep/Baby Sleep/Model/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  Baby Sleep
+//
+//  Created by Aleksandr Serov on 12.07.2020.
+//  Copyright © 2020 Aleksandr Serov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+
+struct Model {
+    var name: String
+    var image: UIImage
+    var audio: String
+}
