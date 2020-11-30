f99906d5a2c566a5eaddd2883654e75ff93d2f6b
Aleksandr <Serov_ae@list.ru>
2020-12-01 00:26:50 +0300

    BBS-6: Refactoring. Removed unused code.

--- Baby Sleep/Baby Sleep/Application/Flow/MainModule/Model/Model.swift
+++ Baby Sleep/Baby Sleep/Application/Flow/MainModule/Model/Model.swift
@@ -1,3 +1,4 @@
+// ----------------------------------------------------------------------------
 //
 //  Model.swift
 //  Baby Sleep
@@ -5,20 +6,14 @@
 //  Created by Aleksandr Serov on 12.07.2020.
 //  Copyright © 2020 Aleksandr Serov. All rights reserved.
 //
+// ----------------------------------------------------------------------------
 
-import Foundation
-import UIKit
 import AVFoundation
 import FirebaseDatabase
+import Foundation
+import UIKit
 
-
-
-struct Model {
-    var name: String
-    var image: UIImage
-    var audio: String
-    var color: natureColor
-}
+// ----------------------------------------------------------------------------
 
 struct Sound: Codable {
     let titleEn: String
@@ -26,8 +21,7 @@ struct Sound: Codable {
     let imageUrl: String
     let audioUrl: String
     var color: Color
-    
-    
+
     init(titleEn: String, titleRu: String, imageUrl: String, audioUrl: String, color: Color) {
         self.titleEn = titleEn
         self.titleRu = titleRu
@@ -35,23 +29,21 @@ struct Sound: Codable {
         self.audioUrl = audioUrl
         self.color = color
     }
-    
+
     init?(snapshot: DataSnapshot) {
         guard let value = snapshot.value as? [String: Any],
-            let titleEn = value["titleEn"] as? String,
-            let titleRu = value["titleRu"] as? String,
-            let imageUrl = value["imageUrl"] as? String,
-            let audioUrl = value["audioUrl"] as? String else { return nil }
-        
+              let titleEn = value["titleEn"] as? String,
+              let titleRu = value["titleRu"] as? String,
+              let imageUrl = value["imageUrl"] as? String,
+              let audioUrl = value["audioUrl"] as? String else { return nil }
+
         let colorSnapshot = snapshot.childSnapshot(forPath: "color")
         guard let color = colorSnapshot.value as? [String: Any],
-            let alpha = color["alpha"] as? Double,
-            let red = color["red"] as? Double,
-            let blue = color["blue"] as? Double,
-            let green = color["green"] as? Double else { return nil }
-        
-        
-        
+              let alpha = color["alpha"] as? Double,
+              let red = color["red"] as? Double,
+              let blue = color["blue"] as? Double,
+              let green = color["green"] as? Double else { return nil }
+
         self.titleEn = titleEn
         self.titleRu = titleRu
         self.imageUrl = imageUrl
@@ -66,7 +58,7 @@ struct Color: Codable {
     var red: Double
     var green: Double
     var blue: Double
-    
+
     init(alpha: Double, red: Double, green: Double, blue: Double ) {
         self.alpha = alpha
         self.red = red
@@ -74,20 +66,3 @@ struct Color: Codable {
         self.blue = blue
     }
 }
-
-enum natureColor {
-    case watterFallColor
-    case forestColor
-    case streamColor
-    case seaColor
-    case rainColor
-    case stormColor
-    case hairdryerColor
-    case whiteNoiseColor
-    case vacuumColor
-    case hoodsColor
-    case carColor
-    
-}
-
-
--- Baby Sleep/Baby Sleep/Application/Flow/StarModule/View/StartViewController.swift
+++ Baby Sleep/Baby Sleep/Application/Flow/StarModule/View/StartViewController.swift
@@ -1,3 +1,4 @@
+// ----------------------------------------------------------------------------
 //
 //  ViewController.swift
 //  Baby Sleep
@@ -5,43 +6,48 @@
 //  Created by Aleksandr Serov on 10.07.2020.
 //  Copyright © 2020 Aleksandr Serov. All rights reserved.
 //
+// ----------------------------------------------------------------------------
 
-import UIKit
-import SnapKit
-import Lottie
 import FirebaseDatabase
+import Lottie
+import SnapKit
+import UIKit
+
+// ----------------------------------------------------------------------------
 
 class StartViewController: UIViewController {
-    
- private let network = NetworkService()
-    
-    //MARK:- UI
+
+    private let network = NetworkService()
+
+    // MARK: - UI
     let startButton = UIButton()
     // View for animation with Lottie
     let backgroundView = AnimationView()
     var sounds: [Sound] = []
-    
+
+    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
- // get model for nature sounds
+        // get model for nature sounds
         network.fetchData { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let sound):
                 self.sounds = sound
                 DispatchQueue.main.async {
-                self.setupButton()
+                    self.configureStartBitton()
                 }
             case .failure(let error):
                 print(error)
             }
         }
-        setupBackgroundView()
+        configureBackgroundView()
     }
-    
-    //MARK:- Methods
-    private func setupButton() {
+
+    // MARK: - Configure UI
+
+    private func configureStartBitton() {
         startButton.backgroundColor = UIColor.buttonColor
         startButton.setTitle("Начать", for: .normal)
         startButton.layer.cornerRadius = 32
@@ -49,7 +55,8 @@ class StartViewController: UIViewController {
         startButton.titleLabel?.font = UIFont(name: "MontserratAlternates-Black", size: 25.0)
         startButton.addTarget(self, action: #selector(startButtonAction), for: .touchUpInside)
         self.view.addSubview(startButton)
-        // set constrains
+
+        // setup constrains
         startButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.bottom.equalToSuperview().inset(182)
@@ -57,28 +64,27 @@ class StartViewController: UIViewController {
             make.leading.trailing.equalToSuperview().inset(50)
         }
     }
-  
-    @objc private func startButtonAction() {
-        let destinationVC = MainViewController()
-        destinationVC.natureSounds = sounds
-        destinationVC.modalPresentationStyle = .fullScreen
-        self.show(destinationVC, sender: nil)
-    }
     
-    private func setupBackgroundView() {
+    private func configureBackgroundView() {
         self.view.addSubview(backgroundView)
         backgroundView.contentMode = .scaleAspectFill
         let animation = Animation.named("background")
         backgroundView.animation = animation
         backgroundView.play()
         backgroundView.loopMode = .loop
-        
+
+        //setup constraints
         backgroundView.snp.makeConstraints { make in
             make.top.leading.trailing.bottom.equalToSuperview()
         }
-        
     }
- 
+
+    // MARK: - Private Methods
+
+    @objc private func startButtonAction() {
+        let destinationVC = MainViewController()
+        destinationVC.natureSounds = sounds
+        destinationVC.modalPresentationStyle = .fullScreen
+        self.show(destinationVC, sender: nil)
+    }
 }
-
-
